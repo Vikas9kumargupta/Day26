39e9ce7f4525782eaf6dcb7dc59415bc4631caee
Vikas9kumargupta <vikas21xxx018@akgec.ac.in>
2023-04-10 14:34:52 +0530

    Fetched Data from API with the help of Retrofit library

---
+++ src/main/java/com/example/day26/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.day26
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var ratingBar: RatingBar
+    lateinit var myAdapter: MyAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getProductData()
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                //if api call is success, then use the data of API and show in your App
+                val responseBody = response.body()
+                val productList = responseBody?.products!!
+
+                myAdapter = MyAdapter(this@MainActivity, productList)
+                recyclerView.adapter = myAdapter
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                //if api call fails
+                Log.d("Main Activity","onFailure"+t.message)
+            }
+        })
+    }
+}
